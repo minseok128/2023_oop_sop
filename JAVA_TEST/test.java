8e84591b7954634d958afcc997a3aa3eed040032
michang <minseok128128@gmail.com>
2023-05-10 22:29:35 +0900

    Test Interface and abstruct class

--- JAVA_TEST/test.java
+++ JAVA_TEST/test.java
@@ -1,28 +1,35 @@
 public class test {
 	public static void main(String[] args)
 	{
-		B b = new B();
-		b.bb();
+		//E d = new E();
 	}
 }
 
-abstract class A {
-	abstract void bb();
+// abstract class A {
+// 	abstract void bb();
+// }
+
+// class B extends A {
+// 	int a;
+
+// 	@Override
+// 	void bb()
+// 	{
+// 		a = 2;
+// 		System.out.println(a);
+// 	}
+// }
+
+// interface in {
+// 	default int add(int a, int b){
+// 		return (a + b);
+// 	}
+// }
+
+
+abstract class C {
+	abstract void add();
 }
 
-class B extends A {
-	int a;
-
-	@Override
-	void bb()
-	{
-		a = 2;
-		System.out.println(a);
-	}
-}
-
-interface in {
-	default int add(int a, int b){
-		return (a + b);
-	}
-}
+// class D extends C {
+// }
