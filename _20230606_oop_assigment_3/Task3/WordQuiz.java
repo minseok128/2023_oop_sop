5f8eec8e5c31709b1338766bb5d83aca10139c8c
michang <minseok128128@gmail.com>
2023-06-06 20:21:52 +0900

    solve task3

--- _20230606_oop_assigment_3/Task3/WordQuiz.java
+++ _20230606_oop_assigment_3/Task3/WordQuiz.java
@@ -1,12 +1,15 @@
 import java.util.Vector;
 import java.util.Scanner;
 import java.util.Random;
-import java.util.Set;
 
 public class WordQuiz {
 	public static void main(String[] args)
 	{
+		var sc = new Scanner(System.in);
+		var rand = new Random();
+		var select_vec = new Vector<Integer>();
 		var v = new Vector<Word>();
+		int correct, select;
 		v.add(new Word("love", "사랑"));
 		v.add(new Word("animal", "동물"));
 		v.add(new Word("error", "오류"));
@@ -20,6 +23,35 @@ public class WordQuiz {
 		v.add(new Word("watermelon", "수박"));
 		v.add(new Word("grape", "포도"));
 		v.add(new Word("strawberry", "딸기"));
+
+		System.out.println("Start English word quessing game. Enter -1 to terminate.");
+		System.out.println("Currently, we have " + v.size() + " words.");
+		while (true)
+		{
+			int rand_num;
+			
+			select_vec.clear();
+			while (select_vec.size() < 4)
+			{
+				rand_num = rand.nextInt(v.size());
+				if (!select_vec.contains(rand_num))
+					select_vec.add(rand_num);
+			}
+			
+			correct = rand.nextInt(4);
+			System.out.println(v.get(select_vec.get(correct)).en + "?");
+			for (int i = 0 ; i < 4 ; i++)
+				System.out.print("(" + (i + 1) + ") " + v.get(select_vec.get(i)).kr + " ");
+			System.out.print(":>");
+			select = sc.nextInt();
+			if (select == correct + 1)
+				System.out.println("Excellent!!");
+			else if (select == -1)
+				break;
+			else
+				System.out.println("No!!");
+		}
+		sc.close();
 	}
 }
 
