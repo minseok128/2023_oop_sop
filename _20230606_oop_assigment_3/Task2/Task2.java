b1b7703e75026d585ddd60b4e25bb6c116e453a0
michang <minseok128128@gmail.com>
2023-06-09 15:18:20 +0900

    inhencement char count

--- _20230606_oop_assigment_3/Task2/Task2.java
+++ _20230606_oop_assigment_3/Task2/Task2.java
@@ -1,7 +1,33 @@
-public class Task2 {
-	public static void main(String[] args)
-	{
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
+public class Task2 {
+	public static void main(String[] args) throws IOException {
+		var bf = new BufferedReader(new InputStreamReader(System.in));
+		int[] histogram = new int[26];
+		String word;
+		String[] arr;
+
+		while (true) {
+			arr = bf.readLine().toUpperCase().split(" ");
+			if (arr[0].equals(";"))
+				break;
+			for (int i = 0; i < arr.length; i++)
+			{
+				word = arr[i];
+				for (int j = 0; j < word.length(); j++)
+				{
+					if (word.charAt(j) >= 'A' && word.charAt(j) <= 'Z')
+						histogram[word.charAt(j) - 'A']++;
+				}
+			}
+		}
+
+		for (int i = 0; i < 26; i++)
+		{
+			System.out.println((char)('A' + i) + ": "+ histogram[i]);
+		}
 	}
 }
 
